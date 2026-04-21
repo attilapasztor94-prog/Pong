e41220c3e3db90ff265740c511caad5ea72c31a5
attil <attila.pasztor94@gmail.com>
2026-04-21 19:44:31 +0300

    Refactor WebSocketConfig si ignorat avertismente IDE

--- src/main/java/org/example/pong/config/WebSocketConfig.java
+++ src/main/java/org/example/pong/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package org.example.pong.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -10,8 +9,14 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Autowired
-    private PongHandler pongHandler;
+    // Am făcut variabila 'final' și am șters @Autowired
+    private final PongHandler pongHandler;
+
+    // Acesta este constructorul.
+    // Spring Boot știe automat să caute PongHandler și să îl bage aici.
+    public WebSocketConfig(PongHandler pongHandler) {
+        this.pongHandler = pongHandler;
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
